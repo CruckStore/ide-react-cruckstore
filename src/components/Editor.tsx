c05864ba99e16e2487f87fea43d23904b872fde0
Cruck Store <cruckstore+github@gmail.com>
2025-05-22 23:37:06 +0200

    Enhance Editor component with autocompletion support and improved styling; add bold font and shadow effect for active state in styles.

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,6 +1,8 @@
-import React from "react";
-import CodeMirror from "@uiw/react-codemirror";
-import { javascript } from "@codemirror/lang-javascript";
+import React from 'react';
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
+import { autocompletion, acceptCompletion } from '@codemirror/autocomplete';
+import { keymap } from '@codemirror/view';
 
 interface EditorProps {
   value: string;
@@ -11,11 +13,17 @@ const Editor: React.FC<EditorProps> = ({ value, onChange }) => (
   <div className="editor">
     <CodeMirror
       value={value}
-      extensions={[javascript()]}
-      editable={true}
+      extensions={[
+        javascript(),
+        autocompletion(),
+        keymap.of([
+          { key: 'Tab', run: acceptCompletion }
+        ])
+      ]}
       onChange={v => onChange(v)}
+      editable={true}
       basicSetup={true}
-      style={{ height: '100%', minHeight: '100%' }}
+      style={{ height: '100%', minHeight: '100%', overflowY: 'auto' }}
     />
   </div>
 );
